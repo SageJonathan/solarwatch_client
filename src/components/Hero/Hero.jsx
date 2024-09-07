fd66202ddf8c271354fcef9cd1a443ff29b80091
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-09-07 10:58:02 -0600

    style nav & hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,11 @@ function Hero() {
   return (
     <div className="hero">
       <p className="hero__header">Your Ultimate Hiking Companion</p>
-      <p className="hero__sub">
-        Plan perfect hikes with precise sunrise and sunset times, and stay
-        prepared with up-to-the-minute weather updates.{" "}
+      <p className="hero__subheader">
+        Plan perfect hikes with precise sunrise and sunset times.
+      </p>
+      <p className="hero__subheader hero__subheader--bottom">
+        Stay prepared with up-to-the-minute weather updates.
       </p>
     </div>
   );

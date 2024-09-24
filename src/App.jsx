b067ec11455eebdab4c86bb28b39bbe078ca98e8
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-09-24 13:49:59 -0600

    remove mountian id route

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,9 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Landing />}></Route> 
-        <Route path="/weather/:mountainId" element={<WeatherDetail/>}></Route>
         <Route path="/weather" element={<WeatherDetail/>}></Route>
       </Routes>
     </BrowserRouter>
   )
 }
-
 export default App;

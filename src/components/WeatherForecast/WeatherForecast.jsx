27f58751c949579823d4ee23504753d335bea760
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-09-25 15:00:45 -0600

    intwine solar and weather to layout

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -3,17 +3,9 @@ import "./WeatherForecast.scss";
 function WeatherForecast (){
     return (
         <>
-        
-        {/* <div className="main__weather-forecast">
-                <h3 className="main__forecast-head" id="main__modifier2">
-                    WEATHER FORECAST
-                </h3>
-
-                <div className="main__forecast-container" id="main__modifier2">
-                    <p className="main__forecast-detail" id="main__modifier3">
-                    </p>  
-                </div>        
-            </div> */}
+        <div className="button__contianer">
+        <button className="button"> Press for weather forecast</button>
+        </div>
         </>
     )
 }

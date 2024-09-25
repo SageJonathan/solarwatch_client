9740b53fa44a2ad6d2294c92fa58904504ea88a7
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-09-25 13:45:09 -0600

    Create jsx structure for solarforecast

--- src/components/SolarForecast/SolarForecast.jsx
+++ src/components/SolarForecast/SolarForecast.jsx
@@ -3,35 +3,87 @@ import "./SolarForecast.scss";
 function SolarFunction ({solarData}){
     return (
         <section className="main">
-        <div className="main__heading-container">
             <h1 className="main__header">
-                Sunrise and Sunset 
+                Solar Activity at ENTER LOCATION
             </h1>
-        </div>
-        <div className="main__information">
-            <div className="main__sunrise" id="main__modifier">
-                <h3 className="main__sunrise-head">
-                    SUNRISE
+        <div className="main__data main__data--row1">
+            <div className="data__container">
+                <h3 className="data__header">
+                    Sunrise
                 </h3>
-                <p className="main__sunrise-time" id="main__modifier3">
+                <p className="data__info">
                     {solarData.sunrise}
                 </p>
             </div>
-            <div className="main__sunset" id="main__modifier">
-                <h3 className="main__sunset-head" id="main__modifier2">
-                    SUNSET
+            <div className="data__container">
+                <h3 className="data__header">
+                    Sunset
                 </h3>
-                <p className="main__sunset-time" id="main__modifier3">
+                <p className="data__info">
                     {solarData.sunset}
                 </p>
             </div>
 
-            <div className="main__sunlight-hours" id="main__modifier">
-                <h3 className="main__sunlight-hours-head" id="main__modifier2">
-                    HOURS
+            <div className="data__container">
+                <h3 className="data__header">
+                    Total Daylight
                 </h3>
-                <p className="main__sunlight-hours-time" id="main__modifier3">
+                <p className="data__info">
                     {solarData.day_length}
+                </p>
+            </div>
+        </div>
+        <div className="main__data main__data--row2">
+        <div className="data__container">
+                <h3 className="data__header">
+                    First Light
+                </h3>
+                <p className="data__info">
+                    {solarData.first_light}
+                </p>
+            </div>
+            <div className="data__container">
+                <h3 className="data__header">
+                    Last Light
+                </h3>
+                <p className="data__info">
+                    {solarData.last_light}
+                </p>
+            </div>
+        </div>
+        <div className="main__data main__data--row3">
+        <div className="data__container">
+                <h3 className="data__header">
+                    Dawn
+                </h3>
+                <p className="data__info">
+                    {solarData.dawn}
+                </p>
+            </div>
+            <div className="data__container">
+                <h3 className="data__header">
+                    Dusk
+                </h3>
+                <p className="data__info">
+                    {solarData.dusk}
+                </p>
+            </div>
+        </div>
+        <div className="main__data main__data--row4">
+        <div className="data__container">
+                <h3 className="data__header">
+                    Solar Noon
+                </h3>
+                <p className="data__info">
+                    {solarData.solar__noon}
+                </p>
+            </div>
+            <div className="data__container">
+                <h3 className="data__header">
+                    Golden Hour
+                </h3>
+                <p className="data__info">
+                    {solarData.golden_hour}
                 </p>
             </div>
         </div>

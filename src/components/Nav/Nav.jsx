e44391c47a099237e740485ef0547cdde0064bb9
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-12-07 15:42:37 -0700

    add home button far right of nav (still linked to link)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,15 @@
 import "./Nav.scss";
 import { Link } from "react-router-dom";
+import homeIcon from "../../assets/icons/home.png"
 
 function Header() {
     return (
         <section className="nav">
             <Link to={`/`}>
             <div className="nav__container">
-                <img className= "nav__img" src="/solarLogo.png" alt="solar watch logo" />
+                <img className= "nav__logo" src="/solarLogo.png" alt="solar watch logo" />
                 <p className="nav__title"> Solar Watch</p>
+                <img src={homeIcon} alt="Go Home" className="nav__home"/>
             </div>
             </Link>
         </section>

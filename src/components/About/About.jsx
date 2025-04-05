7d4ba799075bebdce5b09b1fd80a4f3c340e283d
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-04-05 16:53:26 -0600

    reduce fluff

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,15 @@ import "./About.scss";
 function About() {
   return (
     <div className="about">
-      <p className="about__header">Who are we?</p>
+      {/* <p className="about__header">Who are we?</p> */}
       <p className="about__text">
-        Step into the wild with confidence using <strong>Solar Watch</strong>,
-        the premier app designed for adventurers like you. <br></br>
-        <br></br>Get precise sunrise and sunset times to plan your hikes
-        perfectly, ensuring you never miss a breathtaking dawn or get caught out
-        after dark. But that’s not all! Solar Watch also provides
-        up-to-the-minute weather information, so you’re always prepared for
-        whatever Mother Nature throws your way. Whether you’re scaling mountains
-        or exploring serene trails, Solar Watch is your essential guide to
-        making every hike safe, enjoyable, and unforgettable. 
+        Embark on your adventures with <strong>Solar Watch</strong>, the
+        essential app for outdoor enthusiasts. <br></br>
+        <br></br>Plan your hikes with accurate sunrise and sunset times,
+        ensuring you catch every breathtaking dawn and avoid being caught after
+        dark. Stay prepared with real-time weather updates, so you're ready for
+        whatever nature brings. Whether scaling peaks or exploring trails, Solar
+        Watch is your go-to guide for safe and memorable journeys.
       </p>
     </div>
   );
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,21 +1,21 @@
 import Hero from "../../components/Hero/Hero";
-import About from "../../components/About/About";
 import SearchLocal from "../../components/SearchLocal/SearchLocal";
 import SearchGps from "../../components/SearchGps/SearchGps";
 import SearchAdv from "../../components/SearchAdv/SearchAdv";
+import About from "../../components/About/About";
 import "./Landing.scss";
 
 function Landing() {
   return (
     <>
       <Hero />
-      <About />
-      <h2 className="search__title">Try it out yourself!</h2>
+      <h2 className="search__title">Get Real Time Data</h2>
       <div className="row">
-        <SearchLocal />
-        <SearchGps />
+      <SearchGps />
+      <SearchLocal />
       </div>
       <SearchAdv />
+      <About />
     </>
   );
 }
